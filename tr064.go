b5845a903b581589036148e97a82aa387d7d028e
Holger <hdecarne@gmail.com>
2026-01-19 21:58:25 +0100

    Update Copyright string

--- tr064.go
+++ tr064.go
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 Holger de Carne
+ * Copyright 2024-2026 Holger de Carne
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
